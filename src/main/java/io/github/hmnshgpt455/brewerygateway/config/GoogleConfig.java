1cbbc49669e03209626abd683cf25a2cd4dae77f
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-25 14:41:49 +0530

    Adding localhost route for beer service. Closes #4.

--- src/main/java/io/github/hmnshgpt455/brewerygateway/config/GoogleConfig.java
+++ src/main/java/io/github/hmnshgpt455/brewerygateway/config/GoogleConfig.java
@@ -1,14 +1,12 @@
-package io.github.hmnshgpt455.brewerygateway;
+package io.github.hmnshgpt455.brewerygateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("google")
-public class Config {
+//@Configuration
+//@Profile("google")
+public class GoogleConfig {
 
     @Bean
     public RouteLocator googleConfig(RouteLocatorBuilder builder) {
