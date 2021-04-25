22b81e013a5bca6b948aab4d30f953a432503497
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-25 20:25:42 +0530

    Adding route for beer inventory service. Closes #6.

--- src/main/java/io/github/hmnshgpt455/brewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/io/github/hmnshgpt455/brewerygateway/config/LocalHostRouteConfig.java
@@ -11,13 +11,12 @@ public class LocalHostRouteConfig {
     @Bean
     public RouteLocator locateHostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
-                    .route(r -> r.path("/api/v1/beer/*", "/api/v1/beer*", "/api/v1/beer/upc/*")
+                    .route( r -> r.path("/api/v1/beer/*", "/api/v1/beer*", "/api/v1/beer/upc/*")
                                 .uri("http://localhost:8080"))
-//                    .route(r -> r.path( "/api/v1/customers/*/orders*", "/api/v1/customers*",
-//                            "/api/v1/customers/*/orders/*", "/api/v1/customers/*/orders/*/pickup")
-//                                .uri("http://localhost:8081"))
                     .route(r -> r.path("/api/v1/customers/**")
-                        .uri("http://localhost:8081"))
+                                .uri("http://localhost:8081"))
+                    .route(r -> r.path("/api/v1/beer/*/inventory", "/api/v1/beer/upc/*/inventory")
+                                .uri("http://localhost:8082"))
                     .build();
     }
 }
