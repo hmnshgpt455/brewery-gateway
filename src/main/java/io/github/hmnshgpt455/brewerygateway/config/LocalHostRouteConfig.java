2c86ccfaef9433553eca82f89269a94ab18b0dd6
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-25 15:33:01 +0530

    Adding route for beer order service. Closes #5.

--- src/main/java/io/github/hmnshgpt455/brewerygateway/config/LocalHostRouteConfig.java
+++ src/main/java/io/github/hmnshgpt455/brewerygateway/config/LocalHostRouteConfig.java
@@ -13,6 +13,11 @@ public class LocalHostRouteConfig {
         return routeLocatorBuilder.routes()
                     .route(r -> r.path("/api/v1/beer/*", "/api/v1/beer*", "/api/v1/beer/upc/*")
                                 .uri("http://localhost:8080"))
+//                    .route(r -> r.path( "/api/v1/customers/*/orders*", "/api/v1/customers*",
+//                            "/api/v1/customers/*/orders/*", "/api/v1/customers/*/orders/*/pickup")
+//                                .uri("http://localhost:8081"))
+                    .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
                     .build();
     }
 }
