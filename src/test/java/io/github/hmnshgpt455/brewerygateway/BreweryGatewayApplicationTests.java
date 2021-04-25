b93cfaa5c754fa0c33e80c88fb9c20a823de1350
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-25 13:00:10 +0530

    Adding initial project. Closes #1.

---
+++ src/test/java/io/github/hmnshgpt455/brewerygateway/BreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hmnshgpt455.brewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
