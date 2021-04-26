6135416477a4c3bce1d7863a36d474bccba1d197
hmnshgpt455 <hmnshgpt455@gmail.com>
2021-04-26 20:38:05 +0530

    Add Resilience4J Circuit breaker. Closes #8.

--- src/main/java/io/github/hmnshgpt455/brewerygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/io/github/hmnshgpt455/brewerygateway/config/LoadBalancedRoutesConfig.java
@@ -18,7 +18,12 @@ public class LoadBalancedRoutesConfig {
                 .route(r -> r.path("/api/v1/customers/**")
                         .uri("lb://beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory", "/api/v1/beer/upc/*/inventory")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                        .setFallbackUri("forward:/inventory-failover")
+                                        .setRouteId("inv-failover")))
                         .uri("lb://beer-inventory-service"))
+                .route(r -> r.path("/inventory-failover/**")
+                        .uri("lb://beer-inventory-failover-service"))
                 .build();
     }
 }
